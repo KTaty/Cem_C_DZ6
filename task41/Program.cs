8f0f530a3f0e3b51f65a5ff6b36cd73bef082236
Tatyana_K <tomishka_jvk@mail.ru>
2022-07-12 00:29:54 +0400

    task41

---
+++ task41/Program.cs
@@ -0,0 +1,22 @@
+﻿//Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+int InputAndSumPositiveNumber(int amount)
+{
+    int number = 0;
+    int sum = 0;
+    for (var i = 1; i <= amount; i++)
+    {
+        Console.Write($"Введите {i} число: ");
+        number = Convert.ToInt32(Console.ReadLine());
+        if (number > 0)
+        {
+            sum += number;
+        }
+    }
+    return sum;
+}
+Console.Clear();
+Console.Write("Введите количество чисел: ");
+int amount= Convert.ToInt32(Console.ReadLine());
+int sumPositiveNumber = InputAndSumPositiveNumber(amount);
+Console.WriteLine($"Сумма чисел больших 0 равна {sumPositiveNumber}");
